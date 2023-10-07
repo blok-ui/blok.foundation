a82cba22ceb84caf39527ba08b8ac15b891ccfc2
wartman <faxmaster@gmail.com>
2023-10-07 16:23:44 -0500

    Minor cleanup

--- src/blok/foundation/carousel/CarouselViewport.hx
+++ src/blok/foundation/carousel/CarouselViewport.hx
@@ -2,7 +2,7 @@ package blok.foundation.carousel;
 
 import blok.foundation.animation.*;
 import blok.html.Html;
-import blok.signal.Graph.untrackValue;
+import blok.signal.Graph;
 import blok.ui.*;
 
 using Lambda;
