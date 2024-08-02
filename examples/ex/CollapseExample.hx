c2e87c796c24f856c325907a71357610b0dd623c
wartman <faxmaster@gmail.com>
2024-08-02 12:56:43 -0500

    Latest blok.core

--- examples/ex/CollapseExample.hx
+++ examples/ex/CollapseExample.hx
@@ -1,9 +1,8 @@
 package ex;
 
-import blok.ui.*;
-import blok.html.*;
-import blok.signal.*;
 import blok.foundation.collapse.*;
+import blok.html.*;
+import blok.ui.*;
 import ex.Core;
 
 class CollapseExample extends Component {
@@ -23,23 +22,16 @@ class CollapseExample extends Component {
 
 class ExampleCollapseHeader extends Component {
   @:attribute final child:Child;
+  @:computed final display:String = switch CollapseContext.from(this).status() {
+    case Collapsed: '+';
+    case Expanded: '-';
+  }
 
   function render() {
-    var collapse = CollapseContext.from(this);
     return Html.button({
       className: Typography.fontWeight('bold'),
-      onClick: _ -> collapse.toggle(),
-    },
-      // `collapse.status` is a Signal, so we can observe it
-      // for changes. In a real implementation, this might be
-      // where you have a chevron icon rotate or otherwise
-      // indicate a collapsed/expanded status.
-      child,
-      Text.ofSignal(new Computation(() -> switch collapse.status() {
-        case Collapsed: ' +';
-        case Expanded: ' -';
-      }))
-    );
+      onClick: _ -> CollapseContext.from(this).toggle(),
+    }).child(child, ' ', display);
   }
 }
 
@@ -57,12 +49,14 @@ class ExampleCollapseBody extends Component {
           // work much better, as the padding will be included in
           // when the Component calculates the size of the element.
           .with(Layout.boxSizing('border'))
-      }, Html.div({
-        // Note that we do NOT put the padding in the
-        // main collapse target, as this will result in the collapsed
-        // element still being visible even if its height is `0`.
-        className: Spacing.pad('15px')
-      }, ...(children:Array<Child>)))
+      }).child(
+        Html.div({
+          // Note that we do NOT put the padding in the
+          // main collapse target, as this will result in the collapsed
+          // element still being visible even if its height is `0`.
+          className: Spacing.pad('15px')
+        }).child(children)
+      )
     });
   }
 }
